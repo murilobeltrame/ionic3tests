da1a4467665dd7bfe9c5c26e017294ec1bab0f87
Murilo Beltrame <murilobeltrame@gmail.com>
2019-06-11 19:19:28 -0300

    added viewchild slides reference

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController) {
 
